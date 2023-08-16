a5fde7eb599ae70b54d50f68da8484c6050226c4
maryma00012 <maryma00012@gmail.com>
2023-08-17 03:47:08 +0800

    update index and 404 to allow router in git

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from 'react';
 import { CartContextProvider } from './context/Cart-context';
 import { SearchContextProvider } from './context/Search-context';
@@ -25,7 +25,7 @@ function App() {
     <>
     <SearchContextProvider>
         <CartContextProvider>
-          <HashRouter basename={"/"}>
+          <BrowserRouter>
             <Nav />
             <div className="body">
               <Routes>
@@ -35,7 +35,7 @@ function App() {
                 {/* <Route path={`GodFather`} element={<MovieDetails movieName='GodFather' director='Mohan Raja' movieDesc={`The Godfather is a 1972 American epic crime film[2] directed by Francis Ford Coppola, who co-wrote the screenplay with Mario Puzo, based on Puzo\'s best-selling 1969 novel of the same title. The film stars Marlon Brando, Al Pacino, James Caan, Richard Castellano, Robert Duvall, Sterling Hayden, John Marley, Richard Conte, and Diane Keaton. It is the first installment in The Godfather trilogy, chronicling the Corleone family under patriarch Vito Corleone (Brando) from 1945 to 1955. It focuses on the transformation of his youngest son, Michael Corleone (Pacino), from reluctant family outsider to ruthless mafia boss.`} />} /> */}
               </Routes>
             </div>
-          </HashRouter>
+          </BrowserRouter>
         </CartContextProvider>   
       </SearchContextProvider>
     </>
